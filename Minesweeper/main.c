f8aee4d2180b1e97fe31401ef62ecd5ff8097b34
aidanfoxcsun <aidan.fox.220@my.csun.edu>
2023-09-17 15:26:02 -0700

    Initial Commit

---
+++ Minesweeper/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Minesweeper
+//
+//  Created by Aidan Fox on 9/17/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
